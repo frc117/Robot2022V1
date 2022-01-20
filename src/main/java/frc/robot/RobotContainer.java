61233ebe5dae66305ea915e498cd0c7f4183b5b9
SN74HC32N <>
2022-01-20 15:22:07 -0500

    very insightful comments that make sense

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -27,6 +27,7 @@ public class RobotContainer {
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
+    // garbage yeeeeeeeeee
     configureButtonBindings();
   }
 
